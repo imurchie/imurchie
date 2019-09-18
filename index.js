b67f808a000316325daadef57c17c15aefc614cb
Isaac Murchie <isaac.appium@gmail.com>
2019-09-18 15:46:44 -0400

    fix: use full path

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 
 
-fs.readFile('./info.json', 'utf8', function (err, data) {
+fs.readFile(path.resolve(__dirname, 'info.json'), 'utf8', function (err, data) {
   if (err) {
     return console.log(`Error getting contact info: ${err.message}`);
   }
